aa4b81980f51e687d665cb10c0ec77b39df1da4d
carloeusebi <carloeusebi@gmail.it>
2023-05-15 20:17:18 +0200

    Added Iubenda script

--- assets/footer.php
+++ assets/footer.php
@@ -135,4 +135,11 @@
         <div class="container d-flex align-center">
             <div class="copyright">Federico Dellasanta P.I. 02766970418 | © 2022 tutti i diritti riservati</div>
         </div>
-    </footer>    
+    </footer>
+    
+    <!-- iubenda -->
+    <script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"consentOnContinuedBrowsing":false,"cookiePolicyId":29156312,"countryDetection":true,"floatingPreferencesButtonDisplay":"bottom-right","gdprAppliesGlobally":false,"invalidateConsentWithoutLog":true,"perPurposeConsent":true,"siteId":2614419,"whitelabel":false,"lang":"it", "banner":{ "acceptButtonCaptionColor":"#FFFFFF","acceptButtonColor":"#0073CE","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","closeButtonRejects":true,"customizeButtonCaptionColor":"#4D4D4D","customizeButtonColor":"#DADADA","customizeButtonDisplay":true,"explicitWithdrawal":true,"fontSize":"16px","listPurposes":true,"logo":null,"position":"float-bottom-center","textColor":"#000000","content":"Noi e terze parti selezionate utilizziamo cookie o tecnologie simili per finalità tecniche come specificato nella cookie policy","customizeButtonCaption":"Ulteriori informazioni" }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
