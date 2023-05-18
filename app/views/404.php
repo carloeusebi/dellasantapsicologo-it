c1bcfce582cd685a96c0520ba6255c64a1603878
carloeusebi <carloeusebi@gmail.com>
2023-05-18 16:30:13 +0200

    Update page 404

--- app/views/404.php
+++ app/views/404.php
@@ -1,1 +1,8 @@
-<h1>404 - PAGINA NON TROVATA</h1>
+<?php include 'layouts/head.php'; ?>
+
+
+<div class="text-align-center mt-20">
+<h1 class="p-20">404 - PAGINA NON TROVATA</h1>
+
+    <a class='btn' href="/">torna alla homepage</a>
+</div>
