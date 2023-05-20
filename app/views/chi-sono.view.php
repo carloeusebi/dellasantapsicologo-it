cb6a1b4eda8706f3dd1ea874553de5b1ee6b5383
carloeusebi <carloeusebi@gmail.com>
2023-05-20 18:14:05 +0200

    Fixed pictures responsiveness

--- app/views/chi-sono.view.php
+++ app/views/chi-sono.view.php
@@ -4,14 +4,16 @@ include 'layouts/hero-secondary.php' ?>
 <main>
     <!-- # DR FEDERICO -->
     <section id="dr-federico-dellasanta">
-        <div class="container d-flex-lg">
-            <div class="col-50 d-flex justify-center mb-50 p-20">
-                <img src="img/Federico-Dellasanta-chi-sono.webp" alt="Il dottor Federico Dellasanta">
+        <div class="container d-flex-lg flex-gap20">
+            <div class="col-50 d-flex justify-center mb-50">
+                <figure class="d-flex justify-center">
+                    <img src="img/Federico-Dellasanta-chi-sono.webp" alt="Il dottor Federico Dellasanta">
+                </figure>
             </div>
-            <div class="col-50 p-20">
+            <div class="col-50">
                 <h3>Psicologo clinico cognitivo-comportamentale</h3>
                 <article>
-                    <h2>Dr. Federico Dellasanta</h2>
+                    <h2 class="mb-50">Dr. Federico Dellasanta</h2>
                     <hr class="mb-30">
                     <p class="mb-30">
                         Sono uno <strong>psicologo clinico</strong> e lavoro come libero professionista a <strong>Fano e online</strong>, svolgo attività di <strong>prevenzione, valutazione psicodiagnostica</strong> e <strong>sostegno psicologico</strong> per bambini, adolescenti e adulti.
@@ -19,7 +21,7 @@ include 'layouts/hero-secondary.php' ?>
                     <p class="mb-30">
                         Laureato presso la facoltà di <strong>Psicologia Clinica dell'Università degli Studi di Urbino</strong> e regolarmente iscritto <strong>all'Albo degli Psicologi della regione Marche.</strong>
                     </p>
-                    <p class="mb-30">
+                    <p>
                         Dal 2019, con il desiderio personale e deontologico di portare avanti la formazione e avere sempre più strumenti che possano aiutarmi ad aiutare, sono iscritto alla scuola di specializzazione in <strong>psicoterapia cognitiva e comportamentale Studi Cognitivi di Rimini (ITCC).</strong>
                     </p>
                 </article>
@@ -29,7 +31,7 @@ include 'layouts/hero-secondary.php' ?>
 
     <!-- # IL MIO LAVORO -->
     <section id="il-mio-lavoro">
-        <div class="container d-flex-lg">
+        <div class="container d-flex-lg flex-gap20">
             <div class="col-50 mb-30">
                 <h3>Il mio lavoro</h3>
                 <article>
@@ -40,8 +42,8 @@ include 'layouts/hero-secondary.php' ?>
                 </article>
                 <a href="/cosa-aspettarsi" class="btn">Scopri come posso aiutarti</a>
             </div>
-            <div class="col-50">
-                <img src="img/Federico-Dellasanta-Psicoterapia-e-colloqui-psicologici.webp" alt="">
+            <div class="col-50 d-flex justify-center align-center">
+                <img src="img/Federico-Dellasanta-Psicoterapia-e-colloqui-psicologici.webp" alt="Il dottor Dellasanta che aiuta un paziente">
             </div>
         </div>
     </section>
