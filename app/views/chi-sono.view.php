d9f0f0ffa53ccce931a0f17b459c2de347c6bcbf
carloeusebi <carloeusebi@gmail.com>
2023-05-20 17:20:36 +0200

    Completed 'chi sono' page

--- app/views/chi-sono.view.php
+++ app/views/chi-sono.view.php
@@ -1,7 +1,53 @@
-<?php
-include 'layouts/head.php'; ?>
+<?php include 'layouts/head.php';
+include 'layouts/hero-secondary.php' ?>
 
-<h1>CHI SONO</h1>
+<main>
+    <!-- # DR FEDERICO -->
+    <section id="dr-federico-dellasanta">
+        <div class="container d-flex-lg">
+            <div class="col-50 d-flex justify-center mb-50 p-20">
+                <img src="img/Federico-Dellasanta-chi-sono.webp" alt="Il dottor Federico Dellasanta">
+            </div>
+            <div class="col-50 p-20">
+                <h3>Psicologo clinico cognitivo-comportamentale</h3>
+                <article>
+                    <h2>Dr. Federico Dellasanta</h2>
+                    <hr class="mb-30">
+                    <p class="mb-30">
+                        Sono uno <strong>psicologo clinico</strong> e lavoro come libero professionista a <strong>Fano e online</strong>, svolgo attività di <strong>prevenzione, valutazione psicodiagnostica</strong> e <strong>sostegno psicologico</strong> per bambini, adolescenti e adulti.
+                    </p>
+                    <p class="mb-30">
+                        Laureato presso la facoltà di <strong>Psicologia Clinica dell'Università degli Studi di Urbino</strong> e regolarmente iscritto <strong>all'Albo degli Psicologi della regione Marche.</strong>
+                    </p>
+                    <p class="mb-30">
+                        Dal 2019, con il desiderio personale e deontologico di portare avanti la formazione e avere sempre più strumenti che possano aiutarmi ad aiutare, sono iscritto alla scuola di specializzazione in <strong>psicoterapia cognitiva e comportamentale Studi Cognitivi di Rimini (ITCC).</strong>
+                    </p>
+                </article>
+            </div>
+        </div>
+    </section>
+
+    <!-- # IL MIO LAVORO -->
+    <section id="il-mio-lavoro">
+        <div class="container d-flex-lg">
+            <div class="col-50 mb-30">
+                <h3>Il mio lavoro</h3>
+                <article>
+                    <h2>La Passione nell'aiutare</h2>
+                    <p class="mb-30">
+                        La passione per il mio lavoro mi porta a dedicarmi con il massimo impegno e la massima cura nel sostenere le persone che ricercano la mia assistenza. La mia soddisfazione più grande è sapere di aver cooperato con i miei pazienti e averli aiutati nella loro ricerca del benessere.
+                    </p>
+                </article>
+                <a href="/cosa-aspettarsi" class="btn">Scopri come posso aiutarti</a>
+            </div>
+            <div class="col-50">
+                <img src="img/Federico-Dellasanta-Psicoterapia-e-colloqui-psicologici.webp" alt="">
+            </div>
+        </div>
+    </section>
+
+</main>
+
 
 <?php include "layouts/form.php";
 include "layouts/footer.php" ?>
