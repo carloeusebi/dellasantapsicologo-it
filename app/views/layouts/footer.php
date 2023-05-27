ada103177b62b8a61ab8ce34654a4000c6aabd23
carloeusebi <carloeusebi@gmail.com>
2023-05-27 11:37:02 +0200

    Reactivated Iubenda

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -64,8 +64,37 @@
 <!-- TODO reset iubenda  -->
 
 <!-- iubenda -->
-<!-- <script type="text/javascript">
-var _iub = _iub || [];
-_iub.csConfiguration = {"consentOnContinuedBrowsing":false,"cookiePolicyId":29156312,"countryDetection":true,"floatingPreferencesButtonDisplay":"bottom-right","gdprAppliesGlobally":false,"invalidateConsentWithoutLog":true,"perPurposeConsent":true,"siteId":2614419,"whitelabel":false,"lang":"it", "banner":{ "acceptButtonCaptionColor":"#FFFFFF","acceptButtonColor":"#0073CE","acceptButtonDisplay":true,"backgroundColor":"#FFFFFF","closeButtonRejects":true,"customizeButtonCaptionColor":"#4D4D4D","customizeButtonColor":"#DADADA","customizeButtonDisplay":true,"explicitWithdrawal":true,"fontSize":"16px","listPurposes":true,"logo":null,"position":"float-bottom-center","textColor":"#000000","content":"Noi e terze parti selezionate utilizziamo cookie o tecnologie simili per finalità tecniche come specificato nella cookie policy","customizeButtonCaption":"Ulteriori informazioni" }};
+<script type="text/javascript">
+    var _iub = _iub || [];
+    _iub.csConfiguration = {
+        "consentOnContinuedBrowsing": false,
+        "cookiePolicyId": 29156312,
+        "countryDetection": true,
+        "floatingPreferencesButtonDisplay": "bottom-right",
+        "gdprAppliesGlobally": false,
+        "invalidateConsentWithoutLog": true,
+        "perPurposeConsent": true,
+        "siteId": 2614419,
+        "whitelabel": false,
+        "lang": "it",
+        "banner": {
+            "acceptButtonCaptionColor": "#FFFFFF",
+            "acceptButtonColor": "#0073CE",
+            "acceptButtonDisplay": true,
+            "backgroundColor": "#FFFFFF",
+            "closeButtonRejects": true,
+            "customizeButtonCaptionColor": "#4D4D4D",
+            "customizeButtonColor": "#DADADA",
+            "customizeButtonDisplay": true,
+            "explicitWithdrawal": true,
+            "fontSize": "16px",
+            "listPurposes": true,
+            "logo": null,
+            "position": "float-bottom-center",
+            "textColor": "#000000",
+            "content": "Noi e terze parti selezionate utilizziamo cookie o tecnologie simili per finalità tecniche come specificato nella cookie policy",
+            "customizeButtonCaption": "Ulteriori informazioni"
+        }
+    };
 </script>
-<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script> -->
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8"  async></script>
