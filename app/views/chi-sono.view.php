e285ffe2c10c3e976cd24362609ff6859e4caae6
carloeusebi <carloeusebi@gmail.com>
2023-05-28 20:52:25 +0200

    Validated html

--- app/views/chi-sono.view.php
+++ app/views/chi-sono.view.php
@@ -1,48 +1,45 @@
-<main>
-    <!-- # DR FEDERICO -->
-    <section id="dr-federico-dellasanta">
-        <div class="container d-flex-lg flex-gap20">
-            <div class="col-50 d-flex justify-center mb-50">
-                <figure class="d-flex justify-center">
-                    <img src="img/Federico-Dellasanta-chi-sono.webp" alt="Il dottor Federico Dellasanta">
-                </figure>
-            </div>
-            <div class="col-50">
-                <h3>Psicologo clinico cognitivo-comportamentale</h3>
-                <article>
-                    <h2 class="mb-50">Dr. Federico Dellasanta</h2>
-                    <hr class="mb-30">
-                    <p class="mb-30">
-                        Sono uno <strong>psicologo clinico</strong> e lavoro come libero professionista a <strong>Fano e online</strong>, svolgo attività di <strong>prevenzione, valutazione psicodiagnostica</strong> e <strong>sostegno psicologico</strong> per bambini, adolescenti e adulti.
-                    </p>
-                    <p class="mb-30">
-                        Laureato presso la facoltà di <strong>Psicologia Clinica dell'Università degli Studi di Urbino</strong> e regolarmente iscritto <strong>all'Albo degli Psicologi della regione Marche.</strong>
-                    </p>
-                    <p>
-                        Dal 2019, con il desiderio personale e deontologico di portare avanti la formazione e avere sempre più strumenti che possano aiutarmi ad aiutare, sono iscritto alla scuola di specializzazione in <strong>psicoterapia cognitiva e comportamentale Studi Cognitivi di Rimini (ITCC).</strong>
-                    </p>
-                </article>
-            </div>
+<!-- # DR FEDERICO -->
+<section id="dr-federico-dellasanta">
+    <div class="container d-flex-lg flex-gap20">
+        <div class="col-50 d-flex justify-center mb-50">
+            <figure class="d-flex justify-center">
+                <img src="img/Federico-Dellasanta-chi-sono.webp" alt="Il dottor Federico Dellasanta">
+            </figure>
         </div>
-    </section>
-
-    <!-- # IL MIO LAVORO -->
-    <section id="il-mio-lavoro">
-        <div class="container d-flex-lg flex-gap20">
-            <div class="col-50 mb-30">
-                <h3>Il mio lavoro</h3>
-                <article>
-                    <h2>La Passione nell'aiutare</h2>
-                    <p class="mb-30">
-                        La passione per il mio lavoro mi porta a dedicarmi con il massimo impegno e la massima cura nel sostenere le persone che ricercano la mia assistenza. La mia soddisfazione più grande è sapere di aver cooperato con i miei pazienti e averli aiutati nella loro ricerca del benessere.
-                    </p>
-                </article>
-                <a href="/cosa-aspettarsi" class="btn">Scopri come posso aiutarti</a>
-            </div>
-            <div class="col-50 d-flex justify-center align-center">
-                <img src="img/Federico-Dellasanta-Psicoterapia-e-colloqui-psicologici.webp" alt="Il dottor Dellasanta che aiuta un paziente">
-            </div>
+        <div class="col-50">
+            <h3>Psicologo clinico cognitivo-comportamentale</h3>
+            <article>
+                <h2 class="mb-50">Dr. Federico Dellasanta</h2>
+                <hr class="mb-30">
+                <p class="mb-30">
+                    Sono uno <strong>psicologo clinico</strong> e lavoro come libero professionista a <strong>Fano e online</strong>, svolgo attività di <strong>prevenzione, valutazione psicodiagnostica</strong> e <strong>sostegno psicologico</strong> per bambini, adolescenti e adulti.
+                </p>
+                <p class="mb-30">
+                    Laureato presso la facoltà di <strong>Psicologia Clinica dell'Università degli Studi di Urbino</strong> e regolarmente iscritto <strong>all'Albo degli Psicologi della regione Marche.</strong>
+                </p>
+                <p>
+                    Dal 2019, con il desiderio personale e deontologico di portare avanti la formazione e avere sempre più strumenti che possano aiutarmi ad aiutare, sono iscritto alla scuola di specializzazione in <strong>psicoterapia cognitiva e comportamentale Studi Cognitivi di Rimini (ITCC).</strong>
+                </p>
+            </article>
         </div>
-    </section>
+    </div>
+</section>
 
-</main>
+<!-- # IL MIO LAVORO -->
+<section id="il-mio-lavoro">
+    <div class="container d-flex-lg flex-gap20">
+        <div class="col-50 mb-30">
+            <h3>Il mio lavoro</h3>
+            <article>
+                <h2>La Passione nell'aiutare</h2>
+                <p class="mb-30">
+                    La passione per il mio lavoro mi porta a dedicarmi con il massimo impegno e la massima cura nel sostenere le persone che ricercano la mia assistenza. La mia soddisfazione più grande è sapere di aver cooperato con i miei pazienti e averli aiutati nella loro ricerca del benessere.
+                </p>
+            </article>
+            <a href="/cosa-aspettarsi" class="btn">Scopri come posso aiutarti</a>
+        </div>
+        <div class="col-50 d-flex justify-center align-center">
+            <img src="img/Federico-Dellasanta-Psicoterapia-e-colloqui-psicologici.webp" alt="Il dottor Dellasanta che aiuta un paziente">
+        </div>
+    </div>
+</section>
--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,58 +1,56 @@
     <!-- ! MAIN -->
-    <main>
 
-        <!-- # HERO -->
-        <header id="hero">
-            <div class="container d-flex-lg flex-gap20">
-                <div class="col-50">
-                    <h3>DR. Dellasanta Federico</h3>
-                    <h1>Psicologo Psicoterapeuta <span>Cognitivo Comportamentale</span></h1>
-                    <p class="mb-20">
-                        Mi occupo di consulenze psicologiche, sostegno e propongo percorsi individualizzati a Fano e online.
-                    </p>
-                    <a href="/contatti" class="btn">Prenota un appuntamento</a>
-                </div>
-                <div class="col-50">
-                    <figure class="d-flex justify-center">
-                        <img src="img/new-hero-img.png" alt="Il Dottor Dellasanta">
-                    </figure>
-                </div>
-            </div>
-        </header>
-
-        <!-- # CHI SONO -->
-        <section id="chi-sono" class="text-align-center">
-            <div class="container">
-
-                <h3>chi sono</h3>
-                <h2>Dr. Federico Dellasanta Psicologo</h2>
-                <p class="mb-30">
-                    Pratico la professione di <strong>Psicologo Cognitivo Comportamentale</strong> nel mio studio a Fano e online,
-                    dove svolgo attività di prevenzione, valutazione psicodiagnostica e sostegno psicologico per bambini, adolescenti e adulti.
+    <!-- # HERO -->
+    <header id="hero">
+        <div class="container d-flex-lg flex-gap20">
+            <div class="col-50">
+                <h3>DR. Dellasanta Federico</h3>
+                <h1>Psicologo Psicoterapeuta <span>Cognitivo Comportamentale</span></h1>
+                <p class="mb-20">
+                    Mi occupo di consulenze psicologiche, sostegno e propongo percorsi individualizzati a Fano e online.
                 </p>
-                <a href="/chi-sono" class="btn">Scopri di più</a>
+                <a href="/contatti" class="btn">Prenota un appuntamento</a>
             </div>
-        </section>
-
-        <!-- # COSA ASPETTARSI DALLA TERAPIA -->
-        <section id="cosa-aspettarsi">
-            <div class="container d-flex-md align-center flex-gap20">
-                <div class="col-50">
-                    <h3>cosa aspettarsi dalla terapia</h3>
-                    <h2>L'approccio congitivo-comportamentale</h2>
-                    <p class="mb-30">
-                        La terapia cognitivo comportamentale rappresenta attualmente uno dei modelli per la comprensione e il trattamento dei disturbi psicopatologici ritenuti più efficaci a livello internazionale.
-                    </p>
-                    <a href="/cosa-aspettarsi" class="btn">scopri di più sulla terapia</a>
-                </div>
-                <div class="col-50 tablet">
-                    <figure>
-                        <img src="img/cognitivo-comportamentale-1.svg" alt="Diagramma dell'approccio congitivo-comportamentale">
-                    </figure>
-                </div>
+            <div class="col-50">
+                <figure class="d-flex justify-center">
+                    <img src="img/new-hero-img.png" alt="Il Dottor Dellasanta">
+                </figure>
             </div>
-        </section>
+        </div>
+    </header>
 
-        <!-- # DI COSA MI OCCUPO -->
-        <?php include "layouts/table.php" ?>
-    </main>
+    <!-- # CHI SONO -->
+    <section id="chi-sono" class="text-align-center">
+        <div class="container">
+
+            <h3>chi sono</h3>
+            <h2>Dr. Federico Dellasanta Psicologo</h2>
+            <p class="mb-30">
+                Pratico la professione di <strong>Psicologo Cognitivo Comportamentale</strong> nel mio studio a Fano e online,
+                dove svolgo attività di prevenzione, valutazione psicodiagnostica e sostegno psicologico per bambini, adolescenti e adulti.
+            </p>
+            <a href="/chi-sono" class="btn">Scopri di più</a>
+        </div>
+    </section>
+
+    <!-- # COSA ASPETTARSI DALLA TERAPIA -->
+    <section id="cosa-aspettarsi">
+        <div class="container d-flex-md align-center flex-gap20">
+            <div class="col-50">
+                <h3>cosa aspettarsi dalla terapia</h3>
+                <h2>L'approccio congitivo-comportamentale</h2>
+                <p class="mb-30">
+                    La terapia cognitivo comportamentale rappresenta attualmente uno dei modelli per la comprensione e il trattamento dei disturbi psicopatologici ritenuti più efficaci a livello internazionale.
+                </p>
+                <a href="/cosa-aspettarsi" class="btn">scopri di più sulla terapia</a>
+            </div>
+            <div class="col-50 tablet">
+                <figure>
+                    <img src="img/cognitivo-comportamentale-1.svg" alt="Diagramma dell'approccio congitivo-comportamentale">
+                </figure>
+            </div>
+        </div>
+    </section>
+
+    <!-- # DI COSA MI OCCUPO -->
+    <?php include "layouts/table.php" ?>
