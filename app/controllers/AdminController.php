0ab0c8072d3b794e1da3e5b14e3630259fa2e27e
carloeusebi <carloeusebi@gmail.com>
2023-05-28 20:43:55 +0200

    Fixed admin controllor instructions out of 'else'

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -11,22 +11,29 @@ class AdminController
         $file = __DIR__ . '/../views/admin.view.php';
 
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
             if (isset($_POST['logout'])) {
+
                 $_SESSION = array();
+
                 session_destroy();
+
                 header("Location: /admin");
+
                 exit();
             } else {
-            }
-            $username = $_POST["username"];
-            $password = $_POST["password"];
+                $username = $_POST["username"];
+                $password = $_POST["password"];
 
-            include __DIR__ . '/../config/config.php';
+                include __DIR__ . '/../config/config.php';
 
-            if ($username === $adminUsername && $password === $adminPassword) {
-                $_SESSION['login'] = true;
-            } else {
-                $isInvalid = true;
+                if ($username === $adminUsername && $password === $adminPassword) {
+
+                    $_SESSION['login'] = true;
+                } else {
+
+                    $isInvalid = true;
+                }
             }
         }
 
