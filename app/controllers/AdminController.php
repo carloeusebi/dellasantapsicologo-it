00b2e7a2906e3cef2a9bf2f721fa69742e07996e
carloeusebi <carloeusebi@gmail.com>
2023-05-29 20:06:43 +0200

    Refactored admin methods

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -11,31 +11,43 @@ class AdminController
     {
         $isInvalid = false;
 
+        $admin = new self();
+
         if ($router->getMethod() === "post") {
 
-            if (isset($_POST['logout'])) {
-
-                $_SESSION = array();
-
-                session_destroy();
-
-                header("Location: /admin");
-
-                exit();
-            } elseif (isset($_POST['login'])) {
-                $username = $_POST["username"];
-                $password = $_POST["password"];
-
-                if ($username === Config::$adminUsername && $password === Config::$adminPassword) {
-
-                    $_SESSION['login'] = true;
-                } else {
-
-                    $isInvalid = true;
-                }
-            }
+            if (isset($_POST['logout'])) $admin->logout();
+            elseif (isset($_POST['login'])) $isInvalid = $admin->login();
         }
 
         $router->renderView('admin', ['layout' => 'admin', 'isInvalid' => $isInvalid]);
+    }
+
+    private function logout()
+    {
+        $_SESSION = array();
+
+        session_destroy();
+
+        header("Location: /admin");
+
+        exit();
+    }
+
+    private function login()
+    {
+        $isInvalid = false;
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if ($username === Config::$adminUsername && $password === Config::$adminPassword) {
+
+            $_SESSION['login'] = true;
+        } else {
+
+            $isInvalid = true;
+        }
+
+        return $isInvalid;
     }
 }
