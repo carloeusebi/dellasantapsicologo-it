3af0d642aad0f02cf144efea8179e83c5360c040
carloeusebi <carloeusebi@gmail.com>
2023-06-04 11:58:06 +0200

    Patient page no longer displays id

--- app/views/admin/paziente.view.php
+++ app/views/admin/paziente.view.php
@@ -86,11 +86,13 @@
 </div>
 <ul class="list-unstyled">
     <?php foreach ($labels as $key => $value) : ?>
-        <li>
-            <strong><?= $value ?>:</strong>
-            <?= $patient[$key] ?>
-            <?php if ($key === 'weight' && $patient[$key]) echo 'kg' ?>
-            <?php if ($key === 'height' && $patient[$key]) echo 'cm' ?>
-        </li>
+        <?php if ($key !== 'id') : ?>
+            <li>
+                <strong><?= $value ?>:</strong>
+                <?= $patient[$key] ?>
+                <?php if ($key === 'weight' && $patient[$key]) echo 'kg' ?>
+                <?php if ($key === 'height' && $patient[$key]) echo 'cm' ?>
+            </li>
+        <?php endif ?>
     <?php endforeach ?>
 </ul>
